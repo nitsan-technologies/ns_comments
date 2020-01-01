f723b8724e9c7322582d9695c2849fffa2f15288
NITSAN <sanjay@nitsan.in>
2020-01-01 10:21:21 +0530

    [TODO] Updated demo and documentation description

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ns_comments
 
-- LIVE demo: https://demo.t3terminal.com/t3t-extensions/
+- LIVE demo: https://demo.t3terminal.com/t3t-extensions/general-utility/comments/
 - FREE version: You can download FREE version with basic-features at https://extensions.typo3.org/extension/ns_comments/
-- PRO version: You can download PRO version with more-features & free-support at https://t3terminal.com/comments-typo3-plugin-pro/
+- PRO version: You can download PRO version with more-features & free-support at https://t3terminal.com/comment-plugin-typo3-pages-pro/
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-    'title' => '[NITSAN] Comments plugin for TYPO3',
-    'description' => 'Plug-n-play TYPO3 extension to integrate feature-rich comments features anywhere to your TYPO3 site. This extension includes features like post comment, reply to comment, multi-level comment thread, admin moderation etc., Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/comments-typo3-plugin-pro/',
+    'title' => '[NITSAN] Comments for TYPO3 Pages',
+    'description' => 'Plug-n-play TYPO3 extension to integrate feature-rich comments features anywhere to your TYPO3 site. This extension includes features like post comment, reply to comment, multi-level comment thread, admin moderation etc., Demo: https://demo.t3terminal.com/t3t-extensions/general-utility/comments/ You can download PRO version for more-features & free-support at https://t3terminal.com/comment-plugin-typo3-pages-pro/',
     'category' => 'plugin',
     'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Siddharth Sheth',
     'author_email' => 'sanjay@nitsan.in',
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '8.0.0-10.9.99',

98cf7da740c172682eb3a60e1f6b47abc103e6bd
NITSAN Technologies <sanjay@nitsan.in>
2021-12-31 15:37:37 +0530

    [TASK] Corrected the demo page link

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Comments for TYPO3 Pages',
-    'description' => 'Plug-n-play TYPO3 extension to integrate feature-rich comments features anywhere to your TYPO3 site. This extension includes features like post comment, reply to comment, multi-level comment thread, admin moderation etc., Demo: https://demo.t3terminal.com/t3t-extensions/general-utility/comments/ You can download PRO version for more-features & free-support at https://t3terminal.com/comment-plugin-typo3-pages-pro/',
+    'description' => 'Plug-n-play TYPO3 extension to integrate feature-rich comments features anywhere to your TYPO3 site. This extension includes features like post comment, reply to comment, multi-level comment thread, admin moderation etc., Live-Demo: https://demo.t3terminal.com/t3t-extensions/comments You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-comment-extension-typo3-pages-pro',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies Pvt Ltd',
+    'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',

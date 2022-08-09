9be8f2d65d9d3b3e8787fddabf316f0ad4234c22
NITSAN Technologies <sanjay@nitsan.in>
2022-08-09 10:01:27 +0530

    [TASK] Change URL of t3terminal into t3planet

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Comments for TYPO3 Pages',
-    'description' => 'Plug-n-play TYPO3 extension to integrate feature-rich comments features anywhere to your TYPO3 site. This extension includes features like post comment, reply to comment, multi-level comment thread, admin moderation etc., Live-Demo: https://demo.t3terminal.com/t3t-extensions/comments You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-comment-extension-typo3-pages-pro',
+    'description' => 'Plug-n-play TYPO3 extension to integrate feature-rich comments features anywhere to your TYPO3 site. This extension includes features like post comment, reply to comment, multi-level comment thread, admin moderation etc., Live-Demo: https://demo.t3planet.com/t3t-extensions/comments You can download PRO version for more-features & free-support at https://t3planet.com/typo3-comment-extension-typo3-pages-pro',
     'category' => 'plugin',
     'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',

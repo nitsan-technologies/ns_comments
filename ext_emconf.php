4746844bfafb79907ed20c96fecaa0e4a2775526
NITSAN Technologies <sanjay@nitsan.in>
2023-06-28 20:26:37 +0530

    [TASK] Update dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '12.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.5.99',
+            'typo3' => '12.0.0-12.4.99',
         ],
         'conflicts' => [
         ],

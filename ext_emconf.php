cfdf483fc13c411cf9d74546f78d37fb04b97634
NITSAN <info@nitsan.in>
2025-06-24 12:11:55 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['ns_comments'] = [
-    'title' => 'Pages Comment',
-    'description' => 'The TYPO3 Comment Extension simplifies the process for visitors to leave comments on your website\'s published pages. It utilizes nested comments to create a user-friendly experience. Admins receive email notifications, allowing them to easily manage comments either through email or the backend.
+    'title' => 'TYPO3 Page Comments',
+    'description' => 'Enable user-friendly commenting functionality for TYPO3 pages. Features nested replies, backend moderation, and admin email notifications.',
 
-    *** Live Demo: https://demo.t3planet.com/t3-extensions/comments/ *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-page-comment-extension',
     'category' => 'plugin',
-    'author' => 'T3: Himanshu Ramavat, T3: Nilesh Malankiya, QA: Krishna Dhapa',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',

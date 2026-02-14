1ec9d11d3aaf6739d14ce99f076f57bb9dda7daf
Alfaz-Nitsan-Tech <alfaz.nitsan@gmail.com>
2026-02-14 14:24:04 +0530

    [FEATURE]Add Site Sets

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,10 +12,10 @@ $EM_CONF['ns_comments'] = [
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
-    'version' => '13.0.1',
+    'version' => '14.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-14.0.9',
+            'typo3' => '12.0.0-14.9.99',
         ],
         'conflicts' => [
         ],

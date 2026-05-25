2d63b5ff2341d28df74ea4d4a0a0cca50cb01155
Alfaz-Nitsan-Tech <alfaz.nitsan@gmail.com>
2026-05-25 13:05:49 +0530

    [BUGFIX]Testing Issue Resolved

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,39 +2,20 @@
 
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use Nitsan\NsComments\Controller\CommentController;
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-$versionNumber =  VersionNumberUtility::convertVersionStringToArray(VersionNumberUtility::getCurrentTypo3Version());
-
-if ($versionNumber['version_main'] <= 12) {
-
-    // @extensionScannerIgnoreLine
-    ExtensionUtility::configurePlugin(
-        'ns_comments',
-        'Comment',
-        [
-            CommentController::class => 'list, new, create',
-        ],
-        // non-cacheable actions
-        [
-            CommentController::class => 'list, new, create',
-        ]
-    );
-} else {
-    ExtensionUtility::configurePlugin(
-        'ns_comments',
-        'Comment',
-        [
-            CommentController::class => 'list, new, create',
-        ],
-        // non-cacheable actions
-        [
-            CommentController::class => 'list, new, create',
-        ],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
-}
+ExtensionUtility::configurePlugin(
+    'ns_comments',
+    'Comment',
+    [
+        CommentController::class => 'list, new, create',
+    ],
+    // non-cacheable actions
+    [
+        CommentController::class => 'list, new, create',
+    ],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
